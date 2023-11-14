5ba35344633fabbc36a01992c1003a69fe79a008
Brendon Chui <brendonchui@gmail.com>
2023-11-14 14:04:53 -0800

    Default empty search functionality.

--- src/components/DisplayGrid.tsx
+++ src/components/DisplayGrid.tsx
@@ -1,9 +1,21 @@
-import { SimpleGrid } from "@chakra-ui/react";
-import usePokemonList from "./hooks/usePokemonList";
+import {
+  Box,
+  Center,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+  VStack,
+  filter,
+} from "@chakra-ui/react";
+import usePokemonList, {
+  PkmnResults,
+  PokemonURL,
+} from "./hooks/usePokemonList";
 import PokemonCard from "./PokemonCard";
 import PokemonCardSkeleton from "./PokemonCardSkeleton";
 import useBoundedStore from "./store";
-
+import faceplant from "../assets/pikachu___face_plant_by_vampireselene13_d4p9oo1.png";
 
 const DisplayGrid = () => {
   const { pokemonQuery } = useBoundedStore();
@@ -15,10 +27,46 @@ const DisplayGrid = () => {
 
   if (error) return <p>Error retrieving pokemon...</p>;
 
+  function filteredPokemon(data: PkmnResults | undefined) {
+    const pokemonResults = data?.results
+      .filter((p) => {
+        return pokemonQuery.toLowerCase() === ""
+          ? p
+          : p.name.includes(pokemonQuery.toLowerCase());
+      })
+      .slice(0, 20);
+    return pokemonResults;
+  }
+
   return (
+    // <SimpleGrid
+    //   maxW={"2000px"}
+    //   p={5}
+    //   columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+    //   spacing={5}
+    // >
+    //   {isLoading &&
+    //     skeletons.map((skeleton) => (
+    //       <PokemonCardSkeleton key={skeleton}></PokemonCardSkeleton>
+    //     ))}
+
+    //   {data?.results
+    //     .filter((p) => {
+    //       return pokemonQuery.toLowerCase() === ""
+    //         ? p
+    //         : p.name.includes(pokemonQuery.toLowerCase());
+    //     })
+    //     .slice(0, 20)
+    //     .map((poke, index) => (
+    //       <PokemonCard
+    //         key={index}
+    //         pokemonId={parseInt(poke.url.slice(42, -1))}
+    //       ></PokemonCard>
+    //     ))}
+    // </SimpleGrid>
+
     <SimpleGrid
       maxW={"2000px"}
-      
       p={5}
       columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
       spacing={5}
@@ -28,19 +76,24 @@ const DisplayGrid = () => {
           <PokemonCardSkeleton key={skeleton}></PokemonCardSkeleton>
         ))}
 
-      {data?.results
-        .filter((p) => {
-          return pokemonQuery.toLowerCase() === ""
-            ? p
-            : p.name.includes(pokemonQuery.toLowerCase());
-        })
-        .slice(0, 20)
-        .map((poke, index) => (
+      {filteredPokemon(data)?.length === 0 ||
+      filteredPokemon(data) === undefined ? (
+        <VStack maxW={"1600px"} w={"100%"}>
+          <Image src={faceplant}></Image>
+          <Box>
+
+          </Box>
+          <Heading>Oh no!</Heading>
+          <Text textAlign={'center'}>We can't find what you are looking for...</Text>
+        </VStack>
+      ) : (
+        filteredPokemon(data)?.map((poke, index) => (
           <PokemonCard
             key={index}
             pokemonId={parseInt(poke.url.slice(42, -1))}
           ></PokemonCard>
-        ))}
+        ))
+      )}
     </SimpleGrid>
   );
 };
