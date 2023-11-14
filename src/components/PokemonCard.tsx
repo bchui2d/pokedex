94fbe94801d491081d6217ca6db7d1bea440ed04
Brendon Chui <brendonchui@gmail.com>
2023-11-14 11:33:33 -0800

    Linked to details page.

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,6 +4,8 @@ import {
   HStack,
   Heading,
   Image,
+  LinkBox,
+  LinkOverlay,
 } from "@chakra-ui/react";
 import usePokemon from "./hooks/usePokemon";
 import PokemonTypes from "./PokemonTypes";
@@ -23,40 +25,48 @@ const PokemonCard = ({ pokemonId }: Props) => {
       return str.charAt(0).toUpperCase() + str.slice(1);
     }
     return "";
-  } 
+  }
 
   if (isLoading) return <PokemonCardSkeleton></PokemonCardSkeleton>;
 
   if (error) return <p>Error retrieving pokemon...</p>;
 
   return (
-    <Card
-      as={motion.div}
-      bg={"#f0f1ff"}
-      maxW={"md"}
-      // justifyContent="center"
-      borderRadius={10}
-      overflow={"hidden"}
-      boxShadow={"dark-lg"}
-      whileHover={{ scale: 1.05 }}
-    >
-      <CardBody>
-        <HStack justifyContent={"left"} p={1}>
-          <Image boxSize={"25px"} src={pokeball}></Image>
-          <Heading color={"blackAlpha.700"} fontSize={"2xl"}>
-            {"#" + `0000${data.id}`.slice(-4)}
+    <LinkBox>
+      <Card
+        as={motion.div}
+        bg={"#f0f1ff"}
+        maxW={"md"}
+        // justifyContent="center"
+        borderRadius={10}
+        overflow={"hidden"}
+        boxShadow={"dark-lg"}
+        whileHover={{ scale: 1.05 }}
+      >
+        <CardBody>
+          <HStack justifyContent={"left"} p={1}>
+            <Image boxSize={"25px"} src={pokeball}></Image>
+            <Heading color={"blackAlpha.700"} fontSize={"2xl"}>
+              <LinkOverlay href={"/pokemon/" + data.id}>
+                {"#" + `0000${data.id}`.slice(-4)}
+              </LinkOverlay>
+            </Heading>
+          </HStack>
+          <Image
+            boxSize={"270px"}
+            src={data.sprites.other["official-artwork"].front_default}
+          ></Image>
+          <Heading
+            color={"blackAlpha.700"}
+            textAlign={"center"}
+            fontSize={"2xl"}
+          >
+            {capitalizeFirstLetter(data.name)}
           </Heading>
-        </HStack>
-        <Image
-          boxSize={"270px"}
-          src={data.sprites.other["official-artwork"].front_default}
-        ></Image>
-        <Heading color={"blackAlpha.700"} textAlign={"center"} fontSize={"2xl"}>
-          {capitalizeFirstLetter(data.name)}
-        </Heading>
-        <PokemonTypes types={data.types.map((t) => t.type)}></PokemonTypes>
-      </CardBody>
-    </Card>
+          <PokemonTypes types={data.types.map((t) => t.type)}></PokemonTypes>
+        </CardBody>
+      </Card>
+    </LinkBox>
   );
 };
 
