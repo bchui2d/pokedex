3aae87533a4542bd1b74df7fdee29df06d027c17
Brendon Chui <brendonchui@gmail.com>
2023-11-14 11:41:15 -0800

    Navbar Icon routing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,19 @@
-import { HStack, Heading, Image } from "@chakra-ui/react";
+import { HStack, Heading, Image, LinkBox, LinkOverlay } from "@chakra-ui/react";
 import logo from "../assets/pngegg.png";
 
 const NavBar = () => {
   return (
     <HStack bg={"#9e9fa8"} w={"100%"} maxW={"1600px"} pl={5} pt={2} pb={2}>
-      <Image src={logo} boxSize="45px"></Image>
-      <Heading>PokeDex</Heading>
+      <LinkBox>
+        <LinkOverlay href={"/"}>
+          <Image src={logo} boxSize="45px"></Image>
+        </LinkOverlay>
+      </LinkBox>
+      <LinkBox>
+        <LinkOverlay href={"/"}>
+          <Heading>PokeDex</Heading>
+        </LinkOverlay>
+      </LinkBox>
     </HStack>
   );
 };
